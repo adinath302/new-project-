da6ab5c6f607ed12aab3064ef0318de8a7f4b1e5
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-04-28 00:42:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import Navbar from "./component/Navbar/Navbar.jsx"
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
